a346dcdd76516cbfb8b44b9283d2351bea1e4fe9
jeonbyunghyun <130208955+jeonbyunghyun@users.noreply.github.com>
2025-06-02 18:22:15 +0900

    [Silver III] Title: 프린터 큐, Time: 60 ms, Memory: 38068 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.py
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -1,23 +1,34 @@
 import sys
+import heapq
 from collections import deque
 input = sys.stdin.readline
 
 tc = int(input())
 
 for _ in range(tc):
-    arr = []
+
     N, M = map(int, input().split())
-    ipt = deque(list(map(int, input().split())))
-    ipt[M] += 10
+    input_data = list(map(int, input().split()))
     
-    while(1):
-        max_ipt = max([x % 10 for x in ipt])    
-        if((ipt[0])%10 < max_ipt):
-            ipt.append(ipt[0])
-            ipt.popleft()
-        else:
-            arr.append(ipt[0])
-            ipt.popleft()
-        if(len(ipt) == 0):
-            break
-    print(*[i+1 for i, value in enumerate(arr) if value> 10])
+    queue = deque()
+    heap = []
+    
+    for idx in range(N):
+        queue.append([idx, input_data[idx]])
+        heapq.heappush(heap, -input_data[idx])
+
+    time = 1
+    while(heap and queue):
+        max_value = -heap[0]
+        idx, value = queue.popleft()
+
+        if (value == max_value): # 프린트
+            heapq.heappop(heap)
+            if (idx == M):
+                print(time)
+                break
+            else:
+                time += 1
+        else: # 뒤로 넘김
+            queue.append([idx, value])
+               
